e501e505df6e21408ad7ded2b72421828b3e0b2c
Mike Bloy <mike@bloy.org>
2012-08-12 16:58:32 -0500

    stylesheets

--- config.rb
+++ config.rb
@@ -7,9 +7,9 @@
 # require 'susy'
 
 # Change Compass configuration
-# compass_config do |config|
-#   config.output_style = :compact
-# end
+compass_config do |config|
+  config.output_style = :compact
+end
 
 ###
 # Haml
@@ -24,7 +24,7 @@
 # require 'coffee-filter'
 
 # Automatic image dimensions on image_tag helper
-# activate :automatic_image_sizes
+activate :automatic_image_sizes
 
 ###
 # Page command
@@ -71,14 +71,15 @@
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
   
   # Minify Javascript on build
-  # activate :minify_javascript
+  activate :minify_javascript
   
   # Enable cache buster
-  # activate :cache_buster
+  activate :cache_buster
   
+  activate :directory_indexes
   # Use relative URLs
   # activate :relative_assets
   
