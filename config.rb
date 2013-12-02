4afe7e809a99e49e662bc35ba38b1af7a16c39d7
Mike Bloy <mike@bloy.org>
2013-12-01 19:44:53 -0600

    modernized middleman config

--- config.rb
+++ config.rb
@@ -1,42 +1,28 @@
 require 'dotenv'
+require 'susy'
 Dotenv.load
 
 ###
 # Compass
 ###
 
-# Susy grids in Compass
-# First: gem install compass-susy-plugin
-require 'susy'
-
 # Change Compass configuration
-compass_config do |config|
-  config.output_style = :compact
-end
+# compass_config do |config|
+#   config.output_style = :compact
+# end
 
-###
-# Haml
-###
-
-# CodeRay syntax highlighting in Haml
-# First: gem install haml-coderay
-# require 'haml-coderay'
-
-# CoffeeScript filters in Haml
-# First: gem install coffee-filter
-# require 'coffee-filter'
-
-# Automatic image dimensions on image_tag helper
 activate :automatic_image_sizes
 
 ###
 # Page command
 ###
 
+page "/resume.html", :layout => false
+
 # Per-page layout changes:
 #
 # With no layout
-page "/resume.html", :layout => false
+# page "/path/to/file.html", :layout => false
 #
 # With alternative layout
 # page "/path/to/file.html", :layout => :otherlayout
@@ -55,21 +41,15 @@ page "/resume.html", :layout => false
 # Helpers
 ###
 
-# Methods defined in the helpers block are available in templates
 helpers do
   def extra_title
     (content_for?(:title) ? ": #{yield_content(:title)}" : "")
   end
 end
 
-# Change the CSS directory
-# set :css_dir, "alternative_css_directory"
-
-# Change the JS directory
-# set :js_dir, "alternative_js_directory"
-
-# Change the images directory
-# set :images_dir, "alternative_image_directory"
+set :css_dir, "stylesheets"
+set :js_dir, "javascripts"
+set :images_dir, "images"
 
 activate :deploy do |deploy|
   deploy.build_before = true
@@ -80,27 +60,10 @@ activate :deploy do |deploy|
   deploy.clean = true
 end
 
-# Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
-
-  # Enable cache buster
   activate :cache_buster
-
-  #activate :directory_indexes
-
-  # Use relative URLs
-  # activate :relative_assets
-
-  # Compress PNGs after build
-  # First: gem install middleman-smusher
-  # require "middleman-smusher"
-  # activate :smusher
-
-  # Or use a different image path
-  # set :http_path, "/Content/images/"
+  activate :relative_assets
 end
